10bb73d06e61e963863aba2a48fb92071bafc4a9
hiba rassas <hibarassas12l@gmail.com>
2024-08-04 22:21:27 +0200

    v2

--- Controller/sendMail.js
+++ Controller/sendMail.js
@@ -1,65 +1,67 @@
-const nodemailer = require('nodemailer')
-const {google} = require('googleapis')
-const {OAuth2} = google.auth;
-const OAUTH_PLAYGROUND = 'https://developers.google.com/oauthplayground'
+const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+const { OAuth2 } = google.auth;
+const OAUTH_PLAYGROUND = 'https://developers.google.com/oauthplayground';
 
 const {
     MAILING_SERVICE_CLIENT_ID,
     MAILING_SERVICE_CLIENT_SECRET,
     MAILING_SERVICE_REFRESH_TOKEN,
     SENDER_EMAIL_ADDRESS
-} = process.env
+} = process.env;
 
 const oauth2Client = new OAuth2(
     MAILING_SERVICE_CLIENT_ID,
     MAILING_SERVICE_CLIENT_SECRET,
     MAILING_SERVICE_REFRESH_TOKEN,
     OAUTH_PLAYGROUND
-)
+);
 
-// envoyer mail
-const sendEmail = (to, url, txt) => {
-    oauth2Client.setCredentials({
-        refresh_token: MAILING_SERVICE_REFRESH_TOKEN
-    })
+const sendEmail = async (to, url, txt) => {
+    try {
+        oauth2Client.setCredentials({
+            refresh_token: MAILING_SERVICE_REFRESH_TOKEN
+        });
 
-    const accessToken = oauth2Client.getAccessToken()
-    const smtpTransport = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            type: 'OAuth2',
-            user: SENDER_EMAIL_ADDRESS,
-            clientId: MAILING_SERVICE_CLIENT_ID,
-            clientSecret: MAILING_SERVICE_CLIENT_SECRET,
-            refreshToken: MAILING_SERVICE_REFRESH_TOKEN,
-            accessToken
+        const accessToken = await oauth2Client.getAccessToken();
+        if (!accessToken.token) {
+            throw new Error('Failed to create access token');
         }
-    })
 
-    const mailOptions = {
-        from: SENDER_EMAIL_ADDRESS,
-        to: to,
-        subject: "ONRWEB",
-        html: `
-            <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
-            <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to the ONRWEB.</h2>
-            <p>Congratulations! You're almost set to start using ONRWEB.
-                Just click the button below to validate your email address.
-            </p>
-            
-            <a href=${url} style="background: crimson; text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display: inline-block;">${txt}</a>
-        
-            <p>If the button doesn't work for any reason, you can also click on the link below:</p>
-        
-            <div>${url}</div>
-            </div>
-        `
+        const smtpTransport = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'OAuth2',
+                user: SENDER_EMAIL_ADDRESS,
+                clientId: MAILING_SERVICE_CLIENT_ID,
+                clientSecret: MAILING_SERVICE_CLIENT_SECRET,
+                refreshToken: MAILING_SERVICE_REFRESH_TOKEN,
+                accessToken: accessToken.token
+            }
+        });
+
+        const mailOptions = {
+            from: SENDER_EMAIL_ADDRESS,
+            to: to,
+            subject: "ONRWEB",
+            html: `
+                <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
+                <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to the ONRWEB.</h2>
+                <p>Congratulations! You're almost set to start using ONRWEB.
+                    Just click the button below to validate your email address.
+                </p>
+                <a href=${url} style="background: crimson; text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display: inline-block;">${txt}</a>
+                <p>If the button doesn't work for any reason, you can also click on the link below:</p>
+                <div>${url}</div>
+                </div>
+            `
+        };
+
+        const info = await smtpTransport.sendMail(mailOptions);
+        console.log('Email sent: ' + info.response);
+    } catch (error) {
+        console.error('Error sending email: ', error);
     }
+};
 
-    smtpTransport.sendMail(mailOptions, (err, infor) => {
-        if(err) return err;
-        return infor
-    })
-}
-
-module.exports = sendEmail
+module.exports = sendEmail;
